91d93295b7f4ce8e637a7e9cdba0904deff27efc
nikolai-petukhov <nikolaipetukhov@gmail.com>
2025-09-26 17:52:53 +0300

    events, async

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+from time import perf_counter
 
 
 with sync_playwright() as playwright:
@@ -6,13 +7,25 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False, slow_mo=500)
     # Create a new page
     page = browser.new_page()
+
+    # print("Page loading...")
+    # start = perf_counter()
+
     # Visit the playwright website
-    page.goto("https://bootswatch.com/default")
+    page.goto("https://www.scrapethissite.com/pages/ajax-javascript/")
 
-    #  Locate a link element with "Docs" text
+    link = page.get_by_role("link", name="2015")
+    link.click()
 
-    link = page.locator("a.dropdown-item").first
-    link.click(timeout=2000) # force=True
+    print("... loading oscars for 2015 ...")
+    start = perf_counter()
 
+    page.wait_for_selector(selector="td.film-title")
+
+    time_taken = perf_counter() - start
+    print(f"...movies are loaded, in {round(time_taken, 2)}!")
+
+    # time_taken = perf_counter() - start
+    # print(f"... page loaded in {round(time_taken, 2)}s")
 
     browser.close()
