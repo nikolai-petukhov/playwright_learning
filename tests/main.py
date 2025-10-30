21aad4033aeef9d2d4ea0f33c50dde0ba2ce8d53
nikolai-petukhov <nikolaipetukhov@gmial.com>
2025-10-31 00:36:28 +0300

    google auth

--- tests/main.py
+++ tests/main.py
@@ -1,21 +1,30 @@
 from playwright.sync_api import sync_playwright
+from creds import EMAIL, PASSWORD
 
 
 with sync_playwright() as playwright:
-    # Launch a browser
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    # Create a new page
+    
+    browser = playwright.firefox.launch(headless=False, slow_mo=500)
+    
     page = browser.new_page()
-    # Visit the playwright website
-    page.goto("https://playwright.dev/python")
+    
+    page.goto("https://accounts.google.com")
 
-    #  Locate a link element with "Docs" text
-
-    docs_button = page.get_by_role('link', name="Docs")
-    docs_button.click()
-
-    # Get the url
-    print("Docs: ", page.url)
+    email_input = page.get_by_label("Email or phone")
+    next_button = page.get_by_role("button", name="Next")
+    try_again_button = page.get_by_role("link", name="Try again")
+    password_input = page.get_by_label("Enter your password")
 
 
-    browser.close()
+    while True:
+        email_input.fill(EMAIL)
+        next_button.click()
+
+        if try_again_button:
+            try_again_button.click()
+        else:
+            break
+    
+    next_button.click()
+    password_input.fill(PASSWORD)
+    next_button.click()
