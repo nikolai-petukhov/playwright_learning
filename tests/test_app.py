b654b944a90afce7184a43288137fb0bbbcee741
Nikolai <nikolaipetukhov@gmail.com>
2026-03-08 23:56:29 +0300

    record video

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,27 @@
-from playwright.sync_api import Page
+import pytest
+from playwright.sync_api import Browser, Page
 
 DOCS_URL = "https://playwright.dev/python/docs/intro"
 
-def test_page_has_get_started_link(page: Page):
+@pytest.fixture
+def recordable_page(browser: Browser):
+    context = browser.new_context(
+        record_video_dir="./videos",
+    )
+    page = context.new_page()
+    yield page
+    context.close()
+
+
+def test_page_has_get_started_link(recordable_page: Page) -> None:
+    # Arrange
+    page = recordable_page
+    link_get_started = page.get_by_role("link", name="GET STARTED")
+    # Act
     page.goto("https://playwright.dev/python/")
-    page.screenshot(path="./screenshot.png")
-
-    link = page.get_by_role("link", name="GET STARTED")
-    link.click()
-
+    page.screenshot(path="./screenshots/screenshot.png")
+    link_get_started.click()
     page.wait_for_load_state("networkidle")
-    page.screenshot(path="./docs.png")
-
+    page.screenshot(path="./screenshots/docs.png")
+    # Assert
     assert page.url == DOCS_URL
